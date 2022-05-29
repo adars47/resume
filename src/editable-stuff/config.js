e5ca380c7f99a38ab2ca461677121ab51d88c392
adarsha nepal <adarsha@fritsgo.com>
2022-05-29 09:37:07 +0545

    do not show recent projects

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -60,7 +60,7 @@ const about = {
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
-  show: true,
+  show: false,
   heading: "Recent Projects",
   gitHubUsername: "adars47", //i.e."johnDoe12Gh"
   reposLength: 4,
